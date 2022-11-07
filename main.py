0002a44d709961a32e630672910ccba29040ceee
Nizisi <54412715+Nizisi@users.noreply.github.com>
2022-11-07 00:41:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Program that moves car in set route, when ultrasonic detect something that block its way,car will turn left to avoid it
+Program that moves car in set route, when ultrasonic detect something that block its way,car will turn right to avoid it and comes back
 """
 """
 car movement method:
@@ -15,11 +15,20 @@ import time
 
 class car:
     px = Picarx()
-    speed = int()
-    obstacle_dis = int()
-    angle = int() #control the angle of the car turn
-
+    count = 0
+    def __init__(self, speed, obstacle_dis):
+        self.speed = speed
+        self.obstacle_dis = obstacle_dis
         
+
+    def avoid:
+        for angle in range(0,35):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35,0,-1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+    
  """
 function detect()
 purpose: keep checking whether there are obstacle ahead of the car in set distance
@@ -31,10 +40,11 @@ purpose: keep checking whether there are obstacle ahead of the car in set distan
             print("distance: ",distance)
             if distance > 0 and distance < 300:
                 if distance < obstacle_dis:
-                    px.set_dir_servo_angle(-35)#turn left
+                    avoid()
+                    count +=1
                 else:
                     px.set_dir_servo_angle(0)#no change in direction
-    
+    """
     def route:
         try:
             px.px.forward(speed)
@@ -42,7 +52,7 @@ purpose: keep checking whether there are obstacle ahead of the car in set distan
             for angle in range(0,35):
                 px.set_dir_servo_angle(angle)
                 time.sleep(0.01)
-            for angle in range(0,35):
+            for angle in range(35,0,-1):
                 px.set_dir_servo_angle(angle)
                 time.sleep(0.01)
             for angle in range(0,35):
@@ -53,11 +63,19 @@ purpose: keep checking whether there are obstacle ahead of the car in set distan
                 time.sleep(0.01)
         finally:
             px.forward(0)
-
+    """
+    def route:
+        px.px.forward(speed)
 
 
 if __name__ == "__main__":
+    picar = car(10, 20)
+    
+    while picar.count < 2: # when car do twice avoid motion, stop the car
+        picar.route
+        picar.detect
         
+    picar.speed = 0
 
 
         
